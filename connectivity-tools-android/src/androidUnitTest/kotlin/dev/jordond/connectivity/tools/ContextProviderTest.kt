91aba2e5adc93510dced62ada1a53648c4772b04
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-01-04 15:40:34 -0500

    fix tests for context

--- connectivity-tools-android/src/androidUnitTest/kotlin/dev/jordond/connectivity/tools/ContextProviderTest.kt
+++ connectivity-tools-android/src/androidUnitTest/kotlin/dev/jordond/connectivity/tools/ContextProviderTest.kt
@@ -2,15 +2,14 @@ package dev.jordond.connectivity.tools
 
 import android.content.Context
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.shouldBeInstanceOf
-import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.mockk.every
 import io.mockk.mockk
 import kotlin.test.AfterTest
 import kotlin.test.Test
 
+// The tests seem to fail if we just compare the instances directly, so we need to compare the properties
 class ContextProviderTest {
 
     @AfterTest
@@ -20,25 +19,31 @@ class ContextProviderTest {
 
     @Test
     fun shouldReturnExistingInstanceWhenCreatingMultipleTimes() {
-        val context1 = mockk<Context>()
-        val context2 = mockk<Context>()
+        val context1 = mockk<Context> {
+            every { packageName } returns "com.example"
+        }
+        val context2 = mockk<Context> {
+            every { packageName } returns "com.other"
+        }
 
         val provider1 = ContextProvider.create(context1)
         val provider2 = ContextProvider.create(context2)
 
         provider1 shouldBe provider2
-        provider1.context shouldBe context1
-        provider2.context shouldBe context1
+        provider1.context.packageName shouldBeEqual "com.example"
+        provider2.context.packageName shouldBeEqual "com.example"
     }
 
     @Test
     fun shouldGetInstanceAfterCreation() {
-        val context = mockk<Context>()
+        val context = mockk<Context> {
+            every { packageName } returns "com.example"
+        }
         val created = ContextProvider.create(context)
         val instance = ContextProvider.getInstance()
 
         instance shouldBe created
-        instance.context shouldBe context
+        instance.context.packageName shouldBeEqual "com.example"
     }
 
     @Test
