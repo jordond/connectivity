e9681220c46521aef016e6e0275c7f5eddeeb1b7
Jordon de Hoog <jordon.dehoog@gmail.com>
2026-03-30 14:06:09 -0400

    update deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.gradle.develocity") version "4.4.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 develocity {
